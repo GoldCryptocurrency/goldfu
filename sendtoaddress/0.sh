af32f711a9a2ce71bc086679236a30d68dd3c6e0
ted <asapmoney29@gmail.com>
2017-08-05 15:37:52 +0800

    send to address

---
+++ sendtoaddress/0.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./goldd sendtoaddress GZJ3cU6EFs4XNZFqoSwJX7RYbCYXawGaFa 207297
+./goldd sendtoaddress GeGadpNekZTXkVnJtLWbBC58SmL13FtWWo 248154
+./goldd sendtoaddress GJF9i7vPkxKwYJY4C7SkGcHkzYeKaeRdV4 48069
